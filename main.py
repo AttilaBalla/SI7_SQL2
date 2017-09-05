67dd02be6293d715071f328739667c2b4b59530b
attilaballa <xattus@gmail.com>
2017-09-05 19:51:16 +0200

    Added table headers

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ import queries
 app = Flask(__name__)
 
 
+def makeheaders(listofheaders):
+    result = []
+    for item in listofheaders:
+        result.append(item.replace('_', ' '))
+    return result
+
+
 @app.route('/')
 def index():
     title = "Click on a button display query results"
@@ -16,19 +23,22 @@ def index():
 def mentors():
     title = "Mentors and schools"
     order = ['mentor_name', 'school_name', 'school_country']
+    headers = makeheaders(order)
+    print(headers)
     data = queries.get_mentors_and_schools()
-    return render_template('form.html', title=title, data=data, order=order)
+    return render_template('form.html', title=title, data=data, headers=headers, order=order)
 
 
 @app.route('/all-school')
 def all_school():
     title = "All schools"
     order = ['school_name', 'school_country', 'mentor_name']
+    headers = makeheaders(order)
     data = queries.get_schools_and_mentors()
     for row in data:
         if (row['mentor_name'] == ' '):
             row['mentor_name'] = '- no data -'
-    return render_template('form.html', title=title, data=data, order=order)
+    return render_template('form.html', title=title, data=data, headers=headers, order=order)
 
 
 @app.route('/mentors-by-country')
@@ -36,34 +46,37 @@ def mentors_by_country():
     title = "Mentors by country"
     order = ['country', 'count']
     data = queries.get_mentors_by_country()
-    return render_template('form.html', title=title, data=data, order=order)
+    return render_template('form.html', title=title, data=data, headers=order, order=order)
 
 
 @app.route('/contacts')
 def contacts():
     title = "Contacts"
     order = ['school_name', 'mentor_name']
+    headers = makeheaders(order)
     data = queries.get_contacts()
-    return render_template('form.html', title=title, data=data, order=order)
+    return render_template('form.html', title=title, data=data, headers=headers, order=order)
 
 
 @app.route('/applicants')
 def applicants():
     title = "Recent Applicants"
     order = ['name', 'application_code', 'date']
+    headers = makeheaders(order)
     data = queries.get_applicants()
-    return render_template('form.html', title=title, data=data, order=order)
+    return render_template('form.html', title=title, data=data, headers=headers, order=order)
 
 
 @app.route('/applicants-and-mentors')
 def applicants_and_mentors():
     title = "Assigned Applicants"
     order = ['applicant_name', 'application_code', 'mentor_name']
+    headers = makeheaders(order)
     data = queries.get_applicants_mentors()
     for row in data:
         if (row['mentor_name'] == ' '):
             row['mentor_name'] = '- no data -'
-    return render_template('form.html', title=title, data=data, order=order)
+    return render_template('form.html', title=title, data=data, headers=headers, order=order)
 
 
 if __name__ == "__main__":
